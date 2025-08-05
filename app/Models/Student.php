709a33b222d3710f122d80301c144bfcce1ca969
Precious Julia Kionisala <preciousjkionisala@gmail.com>
2025-08-06 00:49:09 +0800

    This is Student Model

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -10,4 +10,21 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class Student extends Authenticatable
 {
     use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name', 'email', 'password','course_id', 'course', 'company_id', 'faculty_id', 'required_hours'
+    ];
+
+
+    public function company(){
+        return $this->belongsTo(Company::class);
+    }
+
+    public function faculty(){
+        return $this->belongsTo(Faculty::class);
+    }
+
+    public function course(){
+        return $this->belongsTo(Course::class);
+    }
 }
